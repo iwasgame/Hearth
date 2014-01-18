8acb69ca315146fec8ae014629e0eefe650775bb
iwasgame <iwasgame@gmail.com>
2014-01-18 20:47:39 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hearth::Application.config.secret_key_base = '39ce34af030b238a39e673d9f6a2bec448347ac2170a762d57ee04a05b3fd49f165bd95e87faa4a3539e170dd06d98b28a37b65819a144b70d5dcf52466c3de7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hearth::Application.config.session_store :cookie_store, key: '_Hearth_session'
